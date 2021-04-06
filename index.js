3b2deb5470ad059d72992f562d211fb5289980c5
Matt Brown <matthewtbrown124@gmail.com>
2021-04-06 14:38:39 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+var num1 = 62
+var num2 = 1
+var multiply = num1 * num2;
+var random = Math.floor(Math.random() * 10) + 1;
+var num3 = 12
+var num4 = 3
+var mod = num3 / num4;
+var max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
